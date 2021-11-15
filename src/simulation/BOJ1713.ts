693f479a11043aad9a81f08f6ba8f4f9ff6bd0a4
melangyun <melangyun@gmail.com>
2021-11-16 00:16:27 +0900

    solved boj1713

---
+++ src/simulation/BOJ1713.ts
@@ -0,0 +1,68 @@
+export {};
+//https://www.acmicpc.net/problem/1713
+// 후보 추천하기
+
+const inputStrings = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+// 누적 추천횟수, 최초 게시 시간,
+type information = { postTime: number; point: number };
+const photoFrameCnt = Number(inputStrings[0]);
+const recommendations = inputStrings[2].trim().split(" ").map(Number);
+const photoFrames = new Map<number, information>();
+
+solution();
+
+function solution() {
+  for (let i = 0; i < recommendations.length; i++) {
+    addPhoto(recommendations[i], i);
+  }
+  console.log([...photoFrames.keys()].sort().join(" "));
+}
+
+function addPhoto(student: number, time: number) {
+  const postedStudent = photoFrames.get(student);
+  // 이미 게시된 학생의 경우
+  if (postedStudent) {
+    postedStudent.point++;
+    return;
+  }
+
+  // 프레임이 가득 차지 않은 경우
+  if (photoFrames.size !== photoFrameCnt) {
+    photoFrames.set(student, { postTime: time, point: 1 });
+    return;
+  }
+
+  // 프레임이 가득 찬 경우
+  // => 가장 표가 적은 학생
+  // => 동점이라면, 처음 게시된 시간
+  let min = 1000;
+  let minStudents: number[] = [];
+  for (const [student, information] of photoFrames) {
+    if (information.point < min) {
+      min = information.point;
+      minStudents = [student];
+    } else if (information.point === min) {
+      minStudents.push(student);
+    }
+  }
+
+  let deleteStudent = minStudents[0];
+  let postTime = photoFrames.get(deleteStudent)!.postTime;
+
+  for (let i = 1; i < minStudents.length; i++) {
+    const student = minStudents[i];
+    const pt = photoFrames.get(student)!.postTime;
+    if (pt < postTime) {
+      deleteStudent = student;
+      postTime = pt;
+    }
+  }
+
+  photoFrames.delete(deleteStudent);
+  photoFrames.set(student, { postTime: time, point: 1 });
+}
