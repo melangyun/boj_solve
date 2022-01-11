991f186e582e86a28474dfa6387b9515e09fad9a
melangyun <melangyun@gmail.com>
2022-01-12 01:43:56 +0900

    에라토스테네스의 체를 모두 구해놓고 문제를 푸는 방식으로 접근

--- src/math/BOJ9020.py
+++ src/math/BOJ9020.py
@@ -7,33 +7,36 @@ input = sys.stdin.readline
 TEST_CASE = int(input())
 LIMIT = 10000
 
+Erato = [False, False] + [True] * (LIMIT - 1)
+Primes = []
 
-def getGoldBah():
-    gold = int(input())
-    Erato = [False, False] + [True] * (gold - 1)
-    Primes = []
 
-    for i in range(2, gold+1):
+def solveErato():
+    for i in range(2, LIMIT+1):
         if not Erato[i]:
             continue
         Primes.append(i)
 
-        for j in range(2*i, gold+1, i):
+        for j in range(2*i, LIMIT+1, i):
             Erato[j] = False
 
+
+def getGoldBah():
+    num = int(input())
     answerDiff = LIMIT
     for prime in Primes:
-        diff = abs(2*prime-gold)
+        diff = abs(2*prime-num)
         if diff >= answerDiff:
             break
-        if Erato[prime] and Erato[gold-prime] and diff < answerDiff:
-            answer = prime, gold-prime
+        if Erato[prime] and Erato[num-prime] and diff < answerDiff:
+            answer = prime, num-prime
             answerDiff = diff
 
     return str(answer[0]) + " " + str(answer[1])
 
 
 def solution():
+    solveErato()
     answer = []
     for _ in range(TEST_CASE):
         answer.append(getGoldBah())
