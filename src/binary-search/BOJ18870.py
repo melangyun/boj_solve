f64ee378dbdf1af73e09c9a263b5f210c4bc792c
melangyun <melangyun@gmail.com>
2022-01-16 18:21:00 +0900

    solve-BOJ18870(이분탐색)-좌표 압축

---
+++ src/binary-search/BOJ18870.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/18870
+# 좌표 압축
+
+input()
+numbers = list(map(int, input().split()))
+uniqueList = list(set(numbers))
+uniqueList.sort()
+
+
+def solution():
+    answers = []
+    for number in numbers:
+        answers.append(str(binarySearch(0, len(uniqueList)-1, number)))
+    print(" ".join(answers))
+
+
+def binarySearch(start, end, target):
+    while start <= end:
+        mid = (start+end) // 2
+        if uniqueList[mid] == target:
+            return mid
+        elif uniqueList[mid] < target:
+            start = mid + 1
+        else:
+            end = mid-1
+
+    return -1
+
+
+solution()
